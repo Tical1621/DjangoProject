d7ddd97adf48b08971d42de0b6fdd25f7eb4516f
Daniel Ivanov <megaden@users.noreply.github.com>
2022-11-12 12:37:28 +0300

    Added Celery settings

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery
+
+CELERY_BROKER_URL = f'redis://localhost:6379//'
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
