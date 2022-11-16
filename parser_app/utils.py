2e9a3ee3c279f6335997d82bf7c20a7bb3879c0b
Tical1621 <55981055+Tical1621@users.noreply.github.com>
2022-11-16 10:08:00 +0300

    Delete utils.py

--- parser_app/utils.py
+++
@@ -1,7 +0,0 @@
-from urllib.parse import urlparse, parse_qs
-
-
-def parse_url(request):  #parsing groupId from request url
-    url = request.get_full_path()
-    group_id = parse_qs(urlparse(url).query)
-    return group_id
